8a4883f86ab0c9cdc37b66774d4285ffdbf0b3f8
Svitlana3513 <149310522+Svitlana3513@users.noreply.github.com>
2024-03-26 23:42:36 +0000

    Deploying to gh-pages from @ Svitlana3513/goit-js-hw-11@84708442883091f17124dcf6980882f6b0a16911 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,23 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import"./assets/vendor-db7463ae.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();function l(t){const o="https://pixabay.com/api/",i=new URLSearchParams({key:"43044208-9f5714c3237b37b2f1db9f248",q:t,image_type:"photo",orientation:"horizontal",safesearch:!0}),s=`${o}?${i}`;return fetch(s).then(e=>{if(!e.ok)throw new Error(response.status);return e.json()})}function c(t){return`<li class="image-item">
+           <a class="image-link" href="${t.largeImageURL}">
+             <img
+               class="image-photo"
+               width="1280"
+               height="152"
+               src="${t.webformatURL}"
+               data-source="${t.largeImageURL}"
+               alt="${t.tags}"
+             />
+             </a>
+             <ul class="image-description">
+             <li><h3>Likes</h3><p>${t.likes}</p>
+             </li>
+             <li><h3>Views</h3><p>${t.views}</p>
+               </li>
+               <li><h3>Comments</h3><p>${t.comments}</p>
+                 </li>
+                 <li><h3>Downloads</h3><p>${t.downloads}</p>
+                   </li>
+             </ul>
+           </li>`}function a(t){return t.map(c).join("")}l().then(t=>{const o=a(t);console.log(o)});
 //# sourceMappingURL=commonHelpers.js.map
