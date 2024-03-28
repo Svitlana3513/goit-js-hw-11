fffbf77ca474b6cd8731c3ac63c78c0f1b14908e
Svitlana Saharieva <149310522+Svitlana3513@users.noreply.github.com>
2024-03-28 00:04:33 +0000

    add

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,12 +1,13 @@
   
 
 
-function imageTemplate(image) {
-        return `<li class="image-item">
+export const imageTemplate = (images) => {
+  return images.map(image =>
+          `<li class="image-item">
            <a class="image-link" href="${image.largeImageURL}">
              <img
                class="image-photo"
-               width="1280"
+               width="360"
                height="152"
                src="${image.webformatURL}"
                data-source="${image.largeImageURL}"
@@ -23,9 +24,6 @@ function imageTemplate(image) {
                  <li><h3>Downloads</h3><p>${image.downloads}</p>
                    </li>
              </ul>
-           </li>`;
+           </li>`).join('')
 }
 
-export function imagesTemplate(arr) {
-  return arr.map(imageTemplate).join('');
-}; 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,61 @@
 import { getImages } from "./js/pixabay-api";
-import { imagesTemplate } from "./js/render-functions";
+import { imageTemplate } from "./js/render-functions";
 
 import iziToast from 'izitoast';
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css"
 
-getImages().then(images => {
-    const markup = imagesTemplate(images);
-    console.log(markup);
-}
-);
+const inputForm = document.querySelector('.form');
+const imageGallery = document.querySelector('.gallery');
+const imagePreload = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+ 
+inputForm.addEventListener('submit', event => {
+    event.preventDefault();
+    imageGallery.innerHTML = '';
+    const query = event.target.elements.image.value;
+    if (query) {
+        imagePreload.classList.remove('.is-hidden');
+        getImages(query).then(image => {
+            if (!image.hits.length) {
+                iziToast.error({
+                    position: 'topRight',
+                    color: '#EF4040',
+                    messageColor: '#FAFAFB',
+                    maxWidth: '432px',
+                    height: '88',
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                });
+            }
+            const markup = imageTemplate(image.hits);
+            imageGallery.innerHTML = markup;
+            imagePreload.classList.add('.is-hidden');
+            lightbox.refresh();
+        })
+            .catch(error =>
+                console.log(error));
+    } else {
+        iziToast.error({
+                    position: 'topRight',
+                    color: '#EF4040',
+                    messageColor: '#FAFAFB',
+                    maxWidth: '432px',
+                    height: '88',
+                    message: 'Please, fill the form!',
+                });
+        
+    }
+    inputForm.reset();
+    })
+
+
+    
+
+
 
 
