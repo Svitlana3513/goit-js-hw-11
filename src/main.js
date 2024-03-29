b6b16605c48020627338d51aa290cfda4fcf23c3
Svitlana Saharieva <149310522+Svitlana3513@users.noreply.github.com>
2024-03-29 20:51:21 +0000

    fixed

--- src/main.js
+++ src/main.js
@@ -30,33 +30,35 @@ inputForm.addEventListener('submit', event => {
         showLoader();
         getImages(query)
             .then(image => {
-            if (!image.hits.length) {
-                iziToast.error({
-                    position: 'topRight',
-                    color: '#EF4040',
-                    messageColor: '#FAFAFB',
-                    maxWidth: '432px',
-                    height: '88',
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                });
-            }
-            const markup = imageTemplate(image.hits); 
-            imageGallery.insertAdjacentHTML('afterbegin',markup);
-            hideLoader();
-            lightbox.refresh();
-        })
+                if (!image.hits.length) {
+                    iziToast.error({
+                        position: 'topRight',
+                        color: '#EF4040',
+                        messageColor: '#FAFAFB',
+                        maxWidth: '432px',
+                        height: '88',
+                        message: 'Sorry, there are no images matching your search query. Please try again!',
+                    });
+                }
+                const markup = imageTemplate(image.hits);
+                imageGallery.insertAdjacentHTML('afterbegin', markup);
+                hideLoader();
+                lightbox.refresh();
+            })
             .catch(error =>
                 console.log(error));
-                iziToast.error({
-                    position: 'topRight',
-                    color: '#EF4040',
-                    messageColor: '#FAFAFB',
-                    maxWidth: '432px',
-                    height: '88',
-                    message: 'Please, fill the form!',
-                });
+    }else{
+        iziToast.error({
+            title: 'Error',
+            position: 'topRight',
+            color: '#EF4040',
+            messageColor: '#FAFAFB',
+            maxWidth: '432px',
+            height: '88',
+            message: 'Please, fill the form!',
+        });
        
-    } 
+       };
     inputForm.reset();
     })
 
